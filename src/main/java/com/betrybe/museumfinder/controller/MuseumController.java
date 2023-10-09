5b788f4316a4143e412996f05cfa2a9095e0e7ef
Matheus Milanez <mattmila96@gmail.com>
2023-10-09 14:30:49 -0300

    requisito 5

---
+++ src/main/java/com/betrybe/museumfinder/controller/MuseumController.java
@@ -0,0 +1,33 @@
+package com.betrybe.museumfinder.controller;
+
+import com.betrybe.museumfinder.dto.MuseumCreationDto;
+import com.betrybe.museumfinder.dto.MuseumDto;
+import com.betrybe.museumfinder.model.Museum;
+import com.betrybe.museumfinder.service.MuseumServiceInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Camada de controle MuseumController.
+ */
+@RestController
+@RequestMapping("/museums")
+public class MuseumController {
+  private final MuseumServiceInterface service;
+
+  @Autowired
+  public MuseumController(MuseumServiceInterface service) {
+    this.service = service;
+  }
+
+  @PostMapping
+  public ResponseEntity<Museum> createMuseum(@RequestBody Museum museum) {
+    return ResponseEntity.status(HttpStatus.CREATED)
+        .body(service.createMuseum(museum));
+  }
+}
